b4a09d7480b192596a1cd0d3a7b7f1eddead6a2d
verasunyy <verasun@live.com>
2019-05-03 11:29:04 -0700

    update size and position of nyt logo

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 $('#section-select').on('change', function () {
     $(".loading").show();
+    
+// add a class to the header here e.g. .addClass('active');
+
     const section = $(this).val();
     $(".stories").html("");
     if (section !== '') {
         // console.log(selected);
         loadStories(section);
-        // $(".nyt-logo") change logo size
+        // change logo size
+        $(".nyt-logo img").addClass( "logo-img");
+    $(".top").addClass("top-w-article");
     }
 });
 function loadStories(section) {
@@ -14,16 +19,19 @@ function loadStories(section) {
         url: `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=81xlA4cYvKGRAH8yiQiiE2CuHr82DZDM`
     })
         .done(function (data) {
-            // console.log(data);
+            console.log(data);
             // $(".stories").html("");
-            $.each(data.results, function (index, value) {
-                // console.log(index);
+            const validData = data.results.filter(item=> item.multimedia.length==5);
+            $.each(validData, function (index, value) {
+
+                // console.log(index)
                 if (index < 16) {
                     let imageUrl = value.multimedia[4].url;
+                    let articleUrl = value.url;
                     let storyAbstract = value.abstract;
                     // console.log(storyAbstract);
                     // console.log(imageUrl);
-                    $(".stories").append(`<div class="story" style="background-image: url(${imageUrl})"><div class="abstract">${storyAbstract}</div></div>`)
+                    $(".stories").append(`<a href="${articleUrl}" target=blanck class="story" style="background-image: url(${imageUrl})"><div class="abstract">${storyAbstract}</div></a>`)
                 }
             })
         })
